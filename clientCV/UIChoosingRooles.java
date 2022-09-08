0db472833834b9b45b42cbcfc8ca7566a08f29e2
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-09-08 17:49:33 +0200

    Inizializzazione UIChoosingRooles

--- clientCV/UIChoosingRooles.java
+++ clientCV/UIChoosingRooles.java
@@ -2,6 +2,28 @@ package clientCV;
 
 import javax.swing.*;
 
-public class UIChoosingRooles extends JFrame{
-    private JButton centriVaccinaliButton;
+/**
+ * Nell'interfaccia grafica UIChoosingRooles l'utente sceglie se accedere come operatore o come cittadino
+ */
+public class UIChoosingRooles extends JFrame {
+    private JPanel PanelChoosingRooles;
+    private JButton cittadiniButton;
+    private JButton operatoreVaccinaleButton;
+
+    public UIChoosingRooles(String titolo) {
+        super(titolo);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(PanelChoosingRooles);
+        this.pack();
+        this.setSize(1200, 800);
+    }
+
+    public static void main(String[] args) {
+        JFrame frameTitolo = new UIChoosingRooles("VaxCenter");
+        frameTitolo.setVisible(true);
+        frameTitolo.setLocationRelativeTo(null);
+        frameTitolo.setResizable(false);
+
+    }
+
 }
