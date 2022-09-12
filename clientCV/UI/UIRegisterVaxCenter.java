70564930d97ef5182944be10153513011d043bee
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-09-12 18:28:50 +0200

    UIChoosingRoole quasi terminata, manca qualcosa di grafica. Stessa cosa per UIVaccineOperator.
    Inizializzazione della classe UIRegisterVaxCenter

---
+++ clientCV/UI/UIRegisterVaxCenter.java
@@ -0,0 +1,24 @@
+package clientCV.UI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+    /**
+     * La classe UIRegsiterVaxCenter crea l'interfaccia dove l'operatore può inserire un nuovo centro vaccinale
+     *
+     * @author Paolo Bruscagin
+     */
+
+public class UIRegisterVaxCenter extends JFrame implements ActionListener {
+
+    public UIRegisterVaxCenter(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
