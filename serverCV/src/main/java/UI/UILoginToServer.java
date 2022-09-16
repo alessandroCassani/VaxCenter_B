9ccb417c16c58d2abd8a00fafec9f06235eeee7b
alessandroCassani <acassani@studenti.uninsubria.it>
2022-09-16 14:09:05 +0200

    riposizionamento immagine al centro

--- serverCV/src/main/java/UI/UILoginToServer.java
+++ serverCV/src/main/java/UI/UILoginToServer.java
@@ -83,25 +83,10 @@ public class UILoginToServer extends JFrame implements ActionListener {
         this.setSize(1000,600);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         setVisible(true);
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
