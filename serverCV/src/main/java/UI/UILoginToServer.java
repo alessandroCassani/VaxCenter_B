b6bde4b3f187450aaf4b1daf7d6edf631f9afd25
alessandroCassani <acassani@studenti.uninsubria.it>
2022-09-16 13:55:17 +0200

    campi label e textfield grafici inseriti, compreso button login

--- serverCV/src/main/java/UI/UILoginToServer.java
+++ serverCV/src/main/java/UI/UILoginToServer.java
@@ -10,12 +10,20 @@ import java.util.Objects;
 
 public class UILoginToServer extends JFrame implements ActionListener {
 
-    JLabel host = new JLabel();
-    JLabel user = new JLabel();
+    JLabel hostLabel = new JLabel("host:");
 
-    JPanel pnl = new JPanel();
+    JLabel hostName = new JLabel("localhost");
+    JLabel userLabel = new JLabel("username:");
 
-    JPasswordField psw = new JPasswordField();
+    JTextField userTextField = new JTextField();
+
+    JPasswordField pswTextField = new JPasswordField();
+
+    JLabel pswLabel = new JLabel("password:");
+
+    JButton loginButton=new JButton("LOGIN");
+
+    Container container = getContentPane();
 
     public static void main(String[] args) {
         new UILoginToServer();
@@ -23,41 +31,58 @@ public class UILoginToServer extends JFrame implements ActionListener {
 
     public UILoginToServer(){
         Border bordo = new LineBorder(new Color(0x808080, true), 2, true);
-        host.setFont(new Font("Arial",Font.ITALIC,2));
 
-        host.setFont(new Font("Arial",Font.ITALIC,20));
-        host.setText("host: localhost");
-        host.setBounds(400,200,150,20);
-        host.setBorder(bordo);
+        container.setLayout(null);
 
-        user.setFont(new Font("Arial",Font.ITALIC,20));
-        user.setText("user: postgres");
-        user.setBounds(400,250,150,20);
-        user.setBorder(bordo);
+        hostLabel.setFont(new Font("Arial",Font.ITALIC,20));
+        hostLabel.setBounds(600,200,100,20);
+        hostLabel.setBorder(bordo);
 
-        //psw.setFont(new Font("Arial",Font.ITALIC,20));
-        psw.setText("password");
-        psw.setBounds(400,300,150,20);
-        psw.setBorder(bordo);
+        hostName.setFont(new Font("Arial",Font.BOLD,20));
+        hostName.setBounds(750,200,100,20);
+        hostName.setBorder(bordo);
+
+        userLabel.setFont(new Font("Arial",Font.ITALIC,20));
+        userLabel.setBounds(600,250,100,20);
+        userLabel.setBorder(bordo);
+
+        userTextField.setFont(new Font("Arial",Font.BOLD,20));
+        userTextField.setBounds(750,250,100,30);
+        userTextField.setBorder(bordo);
+
+
+        pswLabel.setFont(new Font("Arial",Font.ITALIC,20));
+        pswLabel.setBounds(600,300,100,20);
+        pswLabel.setBorder(bordo);
+
+        pswTextField.setFont(new Font("Arial",Font.BOLD,20));
+        pswTextField.setBounds(750,300,100,30);
+        pswTextField.setBorder(bordo);
+
+        loginButton.setBounds(675,380,90,40);
+
+
+
 
 
         ImageIcon logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/logo.png")));
-        JLabel imagine = new JLabel(logo);
+        JLabel background = new JLabel(logo);
+
+        container.add(hostLabel);
+        container.add(hostName);
+        container.add(userLabel);
+        container.add(pswLabel);
+        container.add(userTextField);
+        container.add(pswTextField);
+        container.add(loginButton);
 
 
-        JLabel sfondo = new JLabel();
-        sfondo.add(user);
-        sfondo.add(host);
-        sfondo.add(psw);
-        sfondo.setIcon(logo);
-        psw.setFont(new Font("Arial",Font.ITALIC,2));
 
-        this.add(sfondo,BorderLayout.CENTER);
         setSize(1000,600);
 
-        this.setTitle("Server VaxCenter");
+        this.setTitle("login to server");
         this.setSize(1000,600);
-        setResizable(true);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
