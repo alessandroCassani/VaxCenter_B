45a0688d4f69baa19fbad1dc2dbe97b60af02845
alessandroCassani <acassani@studenti.uninsubria.it>
2022-09-17 03:04:24 +0200

    aggiunta classe account nel package util, creazione interfaccia server ServerInterface.java

---
+++ serverCV/src/main/java/ServerInterface.java
@@ -0,0 +1,17 @@
+import util.*;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerInterface extends Remote {
+
+    boolean  registraCentroVaccinale(CentroVaccinale centroVaccinale) throws RemoteException;
+
+    boolean registraCittadino(Cittadino cittadino) throws RemoteException;
+
+    boolean registraVacinato(Vaccinato vaccinato) throws RemoteException;
+
+    boolean inserisciEventiAvversi(EventiAvversi eventiAvversi) throws RemoteException;
+
+    boolean signUp(Account account) throws RemoteException;
+}
