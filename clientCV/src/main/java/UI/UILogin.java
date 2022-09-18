86e64ef3c0bf727956fac3b86f98e42fcea45f3c
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-09-18 23:04:11 +0200

    Pulsanti indietro e accedi UICitizen completa

--- clientCV/src/main/java/UI/UILogin.java
+++ clientCV/src/main/java/UI/UILogin.java
@@ -1,6 +1,8 @@
 package UI;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,9 +15,43 @@ import java.util.Objects;
 
 public class UILogin extends JFrame implements ActionListener {
 
+    JButton backToCitizen;
+    JButton accedi;
+
+
     public UILogin(){
+        Border bordo = new LineBorder(new Color(0xFF000000, true), 2, true);
+        Border bordobtn = new LineBorder(new Color(0xFF37C47A, true), 4, true);
+        Border bordobtnInd = new LineBorder(new Color(0xFFF68E3B, true), 4, true);
+
+        ImageIcon ind = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/iconaindietro.png")));
+
+        backToCitizen =  new JButton("     INDIETRO", ind);
+        backToCitizen.setBounds(1075, 670, 350, 120);
+        backToCitizen.setFont(new Font("Georgia", Font.BOLD, 20));
+        backToCitizen.setBackground(new Color(0xFA4723));
+        backToCitizen.setForeground(Color.WHITE);
+        backToCitizen.setBorder(bordobtnInd);
+        backToCitizen.setFocusable(false);
+        backToCitizen.addActionListener(this);
+        backToCitizen.setOpaque(true);
+
+        accedi =  new JButton("ACCEDI");
+        accedi.setBounds(500, 670, 350, 120);
+        accedi.setFont(new Font("Georgia", Font.BOLD, 20));
+        accedi.setBackground(new Color(0x234EFA));
+        accedi.setForeground(Color.WHITE);
+        accedi.setBorder(bordobtn);
+        accedi.setFocusable(false);
+        accedi.addActionListener(this);
+        accedi.setOpaque(true);
+
+
         setBounds(0, 0, 1600, 900);
         setLayout(null);
+
+        add(backToCitizen);
+        add(accedi);
 
         ImageIcon logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/logo.png")));
         setIconImage(logo.getImage());
@@ -30,6 +66,13 @@ public class UILogin extends JFrame implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == accedi) {
+            this.dispose();
+            new UIAdverseEvent();
+        }else if(e.getSource() == backToCitizen) {
+            this.dispose();
+            new UICitizen();
+        }
 
     }
 }
--- clientCV/src/main/java/UI/UISearchVaxCenter.java
+++ clientCV/src/main/java/UI/UISearchVaxCenter.java
@@ -1,6 +1,8 @@
 package UI;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,10 +15,29 @@ import java.util.Objects;
 
 public class UISearchVaxCenter extends JFrame implements ActionListener {
 
+    JButton backToCitizen;
+
     public UISearchVaxCenter(){
+
+        Border bordo = new LineBorder(new Color(0xFF000000, true), 2, true);
+        Border bordobtn = new LineBorder(new Color(0xFF37C47A, true), 4, true);
+        Border bordobtnInd = new LineBorder(new Color(0xFFF68E3B, true), 4, true);
+
+        ImageIcon ind = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/iconaindietro.png")));
+
+        backToCitizen =  new JButton("     INDIETRO", ind);
+        backToCitizen.setBounds(1075, 670, 350, 120);
+        backToCitizen.setFont(new Font("Georgia", Font.BOLD, 20));
+        backToCitizen.setBackground(new Color(0xFA4723));
+        backToCitizen.setForeground(Color.WHITE);
+        backToCitizen.setBorder(bordobtnInd);
+        backToCitizen.setFocusable(false);
+        backToCitizen.addActionListener(this);
+        backToCitizen.setOpaque(true);
 
         setBounds(0, 0, 1600, 900);
         setLayout(null);
+        add(backToCitizen);
 
         ImageIcon logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/logo.png")));
         setIconImage(logo.getImage());
@@ -32,6 +53,10 @@ public class UISearchVaxCenter extends JFrame implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == backToCitizen) {
+            this.dispose();
+            new UICitizen();
+        }
 
     }
 }
