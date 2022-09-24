849d955e3586e4ef0b71d4496635893c6850676d
alessandroCassani <acassani@studenti.uninsubria.it>
2022-09-24 20:04:51 +0200

    aggiunta documentazione

--- clientCV/src/main/java/UI/graphics/RoundJTextField.java
+++ clientCV/src/main/java/UI/graphics/RoundJTextField.java
@@ -20,15 +20,36 @@ public class RoundJTextField extends JPasswordField {
         super(size);
         setOpaque(false);
     }
+
+    /**
+     * metodo che permette la colorazione e la gestione dell'angolatura della parte interna del rettangolo associato alla RoundJTextField
+     * @param g the <code>Graphics</code> object to protect
+     *
+     * @author Alessandro Cassani
+     */
     protected void paintComponent(Graphics g) {
        g.setColor(getBackground());
         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
         super.paintComponent(g);
     }
+
+    /**
+     * metodo che permette la colorazione e la gestione dell'angolatura del bordo del rettangolo associato alla RoundJTextField
+     * @param g  the <code>Graphics</code> context in which to paint
+     *
+     * @author Alessandro Cassaniv
+     */
     protected void paintBorder(Graphics g) {
         g.setColor(new Color(65, 102, 245));
         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
     }
+
+    /**
+     *
+     * @param x   the <i>x</i> coordinata del punto
+     * @param y   the <i>y</i> coordinata del punto
+     * @return true o false in base all'esito del controllo
+     */
     public boolean contains(int x, int y) {
         if (shape == null || !shape.getBounds().equals(getBounds())) {
             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
