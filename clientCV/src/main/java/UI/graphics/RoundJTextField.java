568ed32920681b2d037346628898c5756219a2b9
alessandroCassani <acassani@studenti.uninsubria.it>
2022-09-24 19:59:20 +0200

    modifica rounded corners

--- clientCV/src/main/java/UI/graphics/RoundJTextField.java
+++ clientCV/src/main/java/UI/graphics/RoundJTextField.java
@@ -4,21 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 
+/**
+ * classe che permette la personalizzazione delle textField componenti l'interfaccia grafica
+ *
+ * @author Alessandro Cassani
+ */
 public class RoundJTextField extends JPasswordField {
     private Shape shape;
 
+    /**
+     * costruttore che permettela creazione di un oggetto di tipo RoundJTextField
+     * @param size numero di colonne allocate alla RoundJTextField
+     */
     public RoundJTextField(int size) {
         super(size);
         setOpaque(false);
     }
     protected void paintComponent(Graphics g) {
        g.setColor(getBackground());
-        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 35);
+        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
         super.paintComponent(g);
     }
     protected void paintBorder(Graphics g) {
         g.setColor(new Color(65, 102, 245));
-        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 35);
+        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
     }
     public boolean contains(int x, int y) {
         if (shape == null || !shape.getBounds().equals(getBounds())) {
