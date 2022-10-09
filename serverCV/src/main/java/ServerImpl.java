5aa1c66155424436ee27f0193f30acbfaf509526
LucaPerf <l.perfetti2001@gmail.com>
2022-10-09 19:13:59 +0200

    implementato il metodo isUserRegistrated

--- serverCV/src/main/java/ServerImpl.java
+++ serverCV/src/main/java/ServerImpl.java
@@ -136,7 +136,19 @@ public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
 
     @Override
     public boolean isUserRegistrated(String user) throws RemoteException {
-        return false;
+        try {
+            PreparedStatement ps = DBManagement.getDB().connection.prepareStatement("SELECT * FROM Cittadini_Registrati WHERE username = ? AND password = ?");
+
+            ps.setString(1, user);
+
+            ResultSet resultSet = ps.executeQuery();
+            if(resultSet.next()){
+                return false;
+            }
+        }catch (SQLException e){
+            return false;
+        }
+        return true;
     }
 
     @Override
