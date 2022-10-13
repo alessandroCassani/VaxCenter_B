2ab66b9b142369ac0b24a321ad1e36fb7985ecc6
LucaPerf <l.perfetti2001@gmail.com>
2022-10-13 19:05:58 +0200

    modificta la tabella Centri_vaccinali aggiungedo la una chiave esterna

--- serverCV/src/main/java/DBManagement.java
+++ serverCV/src/main/java/DBManagement.java
@@ -156,7 +156,7 @@ public class DBManagement {
                     + "cognome VARCHAR(30),"
                     + "CodiceFiscale CHAR(16),"
                     + "email VARCHAR(30),"
-                    + "username VARCHAR(30),"
+                    + "username VARCHAR(30) REFERENCES Eventi_Avversi,"
                     + "password VARCHAR(30),"
                     + "nomeCentro VARCHAR(30) REFERENCES CentriVaccinali);"
 
