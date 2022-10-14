fee8ad4996d39ff12388bf0459ee72bed7e3bd8a
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-10-14 16:40:32 +0200

    update Login

--- clientCV/src/main/java/UI/UILogin.java
+++ clientCV/src/main/java/UI/UILogin.java
@@ -13,6 +13,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Objects;
 
 
@@ -40,6 +42,21 @@ public class UILogin extends JFrame implements ActionListener {
         ImageIcon logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/logo.png")));
         setIconImage(logo.getImage());
         setResizable(false);
+
+        //Popup "Se sicuro di uscire?"
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                int resp = JOptionPane.showConfirmDialog(null, "Sei sicuro di uscire?",
+                        "Esci?", JOptionPane.YES_NO_OPTION);
+
+                if (resp == JOptionPane.YES_OPTION) {
+                    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    System.exit(0);
+                } else {
+                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                }
+            }
+        });
 
     }
 
