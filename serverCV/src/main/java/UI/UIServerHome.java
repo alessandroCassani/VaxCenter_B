764810213a92f7e1e6600cf905bb084655133193
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-10-16 19:16:13 +0200

    JOptionPane Server

--- serverCV/src/main/java/UI/UIServerHome.java
+++ serverCV/src/main/java/UI/UIServerHome.java
@@ -6,6 +6,8 @@
  import java.awt.*;
  import java.awt.event.ActionEvent;
  import java.awt.event.ActionListener;
+ import java.awt.event.WindowAdapter;
+ import java.awt.event.WindowEvent;
  import java.util.Objects;
 
  /**
@@ -95,6 +97,24 @@
         getContentPane().setBackground(new Color(181, 226, 232));
         setForeground(Color.WHITE);
         setVisible(true);
+
+        //Popup "Se sicuro di uscire?"
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                UIManager.put("OptionPane.yesButtonText", "Si");
+                UIManager.put("OptionPane.noButtonText", "No");
+
+                int resp = JOptionPane.showConfirmDialog(null, "Sei sicuro di uscire?",
+                        "Esci?", JOptionPane.YES_NO_OPTION);
+
+                if (resp == JOptionPane.YES_OPTION) {
+                    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    System.exit(0);
+                } else {
+                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                }
+            }
+        });
     }
 
      /**
