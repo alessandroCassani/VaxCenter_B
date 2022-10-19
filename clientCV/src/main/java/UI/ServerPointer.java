53ada9acd70933e637f0695a12ec1421285d279c
LucaPerf <l.perfetti2001@gmail.com>
2022-10-19 17:19:44 +0200

    creato e implementato il metodo getRegistry()

---
+++ clientCV/src/main/java/UI/ServerPointer.java
@@ -0,0 +1,11 @@
+package UI;
+
+import java.rmi.registry.Registry;
+
+public class ServerPointer {
+    static Registry registry;
+
+    public static Registry getRegistry(){
+        return registry;
+    }
+}
