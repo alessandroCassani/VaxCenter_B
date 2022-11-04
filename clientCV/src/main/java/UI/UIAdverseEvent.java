b9f886393061ae9db3afdda6d20433c4b1bfe6de
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-11-04 11:28:17 +0100

    migliorie ae

--- clientCV/src/main/java/UI/UIAdverseEvent.java
+++ clientCV/src/main/java/UI/UIAdverseEvent.java
@@ -264,7 +264,7 @@ public class UIAdverseEvent extends JFrame implements ActionListener {
         infoC = ServerPointer.getStub().getInfoCittadino(user);
 
 
-        String[][] data = {{nomeEvento, severitàEvento}, {" Mal di testa", info[0]}, {" Febbre", info[1]}, {" Tachicardia", info[2]}, {" Dolori Musc. Art.", info[3]}, {" Linfoadenopatia", info[4]}, {" Crisi Ipertensiva", info[5]}};
+        String[][] data = {{nomeEvento, severitàEvento}, {" Mal di testa", ("       " +info[0])}, {" Febbre", ("       " +info[1])}, {" Tachicardia", ("       " +info[2])}, {" Dolori Musc. Art.", ("       " +info[3])}, {" Linfoadenopatia", ("       " +info[4])}, {" Crisi Ipertensiva", ("       " +info[5])}};
 
         String[] coloumn = {"EVENTO AVVERSO", "SEVERITA'"};
 
@@ -282,27 +282,27 @@ public class UIAdverseEvent extends JFrame implements ActionListener {
         titoloRiepilogo.setFont(new Font("Georgia", Font.BOLD, 20));
         titoloRiepilogo.setBounds(100, 50, 200, 30);
 
-        JLabel nomeUtente = new JLabel("Nome: " + infoC[1]);
+        JLabel nomeUtente = new JLabel("Nome:   " + infoC[1]);
         nomeUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         nomeUtente.setBounds(20, 100, 400, 20);
 
-        JLabel cognomeUtente = new JLabel("Cognome: " + infoC[2]);
+        JLabel cognomeUtente = new JLabel("Cognome:   " + infoC[2]);
         cognomeUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         cognomeUtente.setBounds(20, 160, 400, 20);
 
-        JLabel codiceFiscaleUtente = new JLabel("Codice Fiscale: " + infoC[3]);
+        JLabel codiceFiscaleUtente = new JLabel("Codice Fiscale:   " + infoC[3]);
         codiceFiscaleUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         codiceFiscaleUtente.setBounds(20, 220, 400, 20);
 
-        JLabel emailUtente = new JLabel("Email: " + infoC[4]);
+        JLabel emailUtente = new JLabel("Email:   " + infoC[4]);
         emailUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         emailUtente.setBounds(20, 280, 400, 20);
 
-        JLabel UserIDUtente = new JLabel("UserID: " + infoC[5]);
+        JLabel UserIDUtente = new JLabel("UserID:   " + infoC[5]);
         UserIDUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         UserIDUtente.setBounds(20, 340, 400, 20);
 
-        JLabel IDUnivocoUtente = new JLabel("ID Univoco: " + infoC[0]);
+        JLabel IDUnivocoUtente = new JLabel("ID Univoco:   " + infoC[0]);
         IDUnivocoUtente.setFont(new Font("Georgia", Font.BOLD, 15));
         IDUnivocoUtente.setBounds(20, 400, 400, 20);
 
