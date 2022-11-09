bbb8b15dd4d71051494bdab0395cff476d449781
alessandroCassani <acassani@studenti.uninsubria.it>
2022-11-09 16:44:01 +0100

    aggiunta classe cipher e tabella ciprher nella creazione

---
+++ serverCV/src/main/java/database/Cipher.java
@@ -0,0 +1,64 @@
+package database;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Cipher {
+
+    final String ALGORITHM = "AES/CBC/PKCS5Padding";
+
+    static SecretKey key = null;
+
+    static IvParameterSpec iv = null;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey key = keyGenerator.generateKey();
+        String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        System.out.println(encodedKey);
+    }
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+
+    public static String encrypt(String algorithm, String input, SecretKey key,
+                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance(algorithm);
+        cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder()
+                .encodeToString(cipherText);
+    }
+
+    public static String decrypt(String algorithm, String cipherText, SecretKey key,
+                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance(algorithm);
+        cipher.init(javax.crypto.Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                .decode(cipherText));
+        return new String(plainText);
+    }
+}
