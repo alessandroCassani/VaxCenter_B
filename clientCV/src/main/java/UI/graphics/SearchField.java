12f3017fac07c33268a57f9a4c21f6a79dae5578
Damiano <damianoficara@gmail.com>
2022-11-12 19:46:29 +0100

    javadoc SearchField

--- clientCV/src/main/java/UI/graphics/SearchField.java
+++ clientCV/src/main/java/UI/graphics/SearchField.java
@@ -33,45 +33,120 @@ import UI.graphics.InfoSearchEvent;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 
+/**
+ * La classe SearchField è responsabile della gestione grafica del campo di ricerca
+ @author Damiano Ficara
+ */
 public class SearchField extends JTextField {
 
+    /**
+     * metodo che consente di ottenere il suggerimento del campo di ricerca
+     * @return stringa rappresentante il suggerimento
+     * @author Damiano Ficara
+     */
     public String getHint() {
         return hint;
     }
 
+    /**
+     * metodo che consente di impostare il suggerimento del campo di ricerca
+     * @return stringa rappresentante il nuovo suggerimento
+     * @author Damiano Ficara
+     */
     public void setHint(String hint) {
         this.hint = hint;
     }
 
+
+    /**
+     * metodo che consente di ottenere il primo colore di sovrapposizione
+     * @return colore di sovrapposizione
+     * @author Damiano Ficara
+     */
     public Color getColorOverlay1() {
         return colorOverlay1;
     }
 
+    /**
+     * metodo che consente di impostare il primo colore di sovrapposizione
+     * @return nuovo colore di sovrapposizione
+     * @author Damiano Ficara
+     */
     public void setColorOverlay1(Color colorOverlay1) {
         this.colorOverlay1 = colorOverlay1;
     }
 
+    /**
+     * metodo che consente di ottenere il secondo colore di sovrapposizione
+     * @return colore di sovrapposizione
+     * @author Damiano Ficara
+     */
     public Color getColorOverlay2() {
         return colorOverlay2;
     }
 
+    /**
+     * metodo che consente di impostare il secondo colore di sovrapposizione
+     * @return nuovo colore di sovrapposizione
+     * @author Damiano Ficara
+     */
     public void setColorOverlay2(Color colorOverlay2) {
         this.colorOverlay2 = colorOverlay2;
     }
 
+    /**
+     * Variabile che rappresenta l'animazione alla selezione della tipologia di ricerca
+     */
     private Animator animator;
+    /**
+     * Variabile di appoggio allo scorrimento della barra di animazione
+     */
     private float animate;
+    /**
+     * Variabile che identifica la selezione o meno della tipologia di ricerca
+     */
     private boolean option = false;
+    /**
+     * Variabile responsabile della gestione della forma in uso
+     */
     private Shape shape;
+    /**
+     * Variabile che identifica la selezione o meno dell'evento collegato all'utilizzo del cursore
+     */
     private boolean mousePressed = false;
+    /**
+     * Lista di oggetti di <code>InfoSearch</code>
+     */
     private final List<InfoSearch> items = new ArrayList<>();
+    /**
+     * Lista di oggetti di <code>InfoSearchEvent</code>
+     */
     private final List<InfoSearchEvent> events = new ArrayList<>();
+    /**
+     * Variabile che identifica indice delle tipologia di ricerca presenti
+     */
     private int selectedIndex = -1;
+    /**
+     * Variabile che identifica indice del tipo di ricerca scelta
+     */
     private int pressedIndex = -1;
+    /**
+     * Primo colore di sovrapposizione
+     */
     private Color colorOverlay1 = new Color(40, 170, 240);
+    /**
+     * Secondo colore di sovrapposizione
+     */
     private Color colorOverlay2 = new Color(138, 39, 232);
+    /**
+     * Stringa rappresentante il suggerimento di default
+     */
     private String hint = "Search...";
 
+    /**
+     * Costruttore responsabile dell'inizializzazione dei componenti grafici, dell'animazione e degli eventi collegati al campo di ricerca
+     * @author Damiano Ficara
+     */
     public SearchField() {
         setBorder(new EmptyBorder(10, 10, 10, 40));
         setSelectionColor(new Color(25, 141, 255));
@@ -131,7 +206,10 @@ public class SearchField extends JTextField {
         });
         initAnimator();
     }
-
+    /**
+     * metono che permette di aggiungere un'opzione di ricerca
+     * @author Damiano Ficara
+     */
     public void addOption(InfoSearch option) {
         items.add(option);
         if (selectedIndex == -1) {
@@ -140,10 +218,19 @@ public class SearchField extends JTextField {
         }
     }
 
+    /**
+     * metono che permette di aggiungere le specifiche di un tipo di ricerca
+     * @author Damiano Ficara
+     */
     public void addEventOptionSelected(InfoSearchEvent event) {
         events.add(event);
     }
 
+    /**
+     * metono che permette di ottenere l'oggetto <code>InfoSearch</code>  della ricerca scelta
+     * @author Damiano Ficara
+     * @return tipologia di ricerca scelta
+     */
     public InfoSearch getSelectedOption() {
         if (selectedIndex == -1) {
             return null;
@@ -152,26 +239,48 @@ public class SearchField extends JTextField {
         }
     }
 
+    /**
+     * metono che permette di analizzare se è stata scelta una specifica modalità di ricerca
+     * @author Damiano Ficara
+     * @return true o false selezione
+     */
     public boolean isSelected() {
         return selectedIndex >= 0;
     }
 
+    /**
+     * metono che permette evidenziare la ricerca selezionata
+     * @author Damiano Ficara
+     */
     public void setSelectedIndex(int index) {
         selectedIndex = index;
         runEvent();
         repaint();
     }
 
+    /**
+     * metono che permette di ottenere indice della ricerca selezionata
+     * @author Damiano Ficara
+     * @return indice rappresentante la selezione
+     */
     public int getSelectedIndex() {
         return selectedIndex;
     }
 
+    /**
+     * metono privato che permette di gestire gli eventi collegati alla ricerca
+     * @author Damiano Ficara
+     */
     private void runEvent() {
         for (InfoSearchEvent event : events) {
             event.optionSelected(getSelectedOption(), selectedIndex);
         }
     }
 
+    /**
+     * metono privato che permette di gestire gli eventi collegati alle animazioni
+     * @author Damiano Ficara
+     */
     private void startAnimate() {
         if (animator.isRunning()) {
             float f = animator.getTimingFraction();
@@ -184,6 +293,10 @@ public class SearchField extends JTextField {
         animator.start();
     }
 
+    /**
+     * metono privato che si occupa di gestire gli eventi collegati alla posizione del cursore
+     * @author Damiano Ficara
+     */
     private boolean isOver(Point mouse) {
         if (!option) {
             return shape.contains(mouse);
@@ -191,6 +304,11 @@ public class SearchField extends JTextField {
         return false;
     }
 
+    /**
+     * metono privato che si occupa di gestire gli eventi collegati all'eventuale pressione del cursore
+     * @author Damiano Ficara
+     * @return indice tipologia di ricerca
+     */
     private int checkPress(Point mouse) {
         int index = -1;
         if (!items.isEmpty() && option) {
@@ -204,7 +322,10 @@ public class SearchField extends JTextField {
         }
         return index;
     }
-
+    /**
+     * metono privato che inizializza l'animazione
+     * @author Damiano Ficara
+     */
     private void initAnimator() {
         animator = new Animator(500, new TimingTargetAdapter() {
             @Override
@@ -227,6 +348,11 @@ public class SearchField extends JTextField {
         animator.setAcceleration(0.5f);
     }
 
+    /**
+     * Metodo responsabile dell'operazione di disegno del componente grafico
+     * @param grphcs componente grafico considerato
+     * @author Damiano Ficara
+     */
     @Override
     protected void paintComponent(Graphics grphcs) {
         Graphics2D g2 = (Graphics2D) grphcs.create();
@@ -240,6 +366,11 @@ public class SearchField extends JTextField {
         super.paintComponent(grphcs);
     }
 
+    /**
+     * Metodo responsabile dell'operazione di disegno
+     * @param grphcs componente grafico considerato
+     * @author Damiano Ficara
+     */
     @Override
     public void paint(Graphics grphcs) {
         super.paint(grphcs);
@@ -264,7 +395,11 @@ public class SearchField extends JTextField {
         drawItem(g2, x, y, getWidth() - 2, height);
         g2.dispose();
     }
-
+    /**
+     * Metodo responsabile dell'operazione di disegno del suggerimento
+     * @param g2 componente grafico considerato
+     * @author Damiano Ficara
+     */
     private void paintHint(Graphics2D g2) {
         if (getText().length() == 0) {
             int h = getHeight();
@@ -278,7 +413,15 @@ public class SearchField extends JTextField {
             g2.drawString(hint, ins.left, h / 2 + fm.getAscent() / 2 - 2);
         }
     }
-
+    /**
+     * Metodo responsabile dell'operazione di disegno dell'oggetto di ricerca consierato
+     * @param g2 componente grafico considerato
+     * @param x cordinata dell'oggetto
+     * @param y cordinata dell'oggetto
+     * @param width dimensione dell'oggetto di ricerca
+     * @param height dimensione dell'oggetto di ricerca
+     * @author Damiano Ficara
+     */
     private void drawItem(Graphics2D g2, double x, double y, double width, double height) {
         double w = width - x;
         double per = w / items.size();
@@ -287,6 +430,16 @@ public class SearchField extends JTextField {
         }
     }
 
+    /**
+     * Metodo responsabile dell'operazione di disegno dell'icona dell'oggetto di ricerca consierato
+     * @param g2 componente grafico considerato
+     * @param x cordinata dell'oggetto
+     * @param y cordinata dell'oggetto
+     * @param width dimensione dell'oggetto di ricerca
+     * @param height dimensione dell'oggetto di ricerca
+     * @param index indice dell'oggetto di ricerca a cui assegnare icona
+     * @author Damiano Ficara
+     */
     private void drawIcon(Graphics2D g2, double x, double y, double width, double height, int index) {
         Composite oldComposite = g2.getComposite();
         if (index != selectedIndex) {
@@ -302,6 +455,11 @@ public class SearchField extends JTextField {
         g2.setComposite(oldComposite);
     }
 
+    /**
+     * Metodo che permette di ottenere l'immagine avendo a disposizione indice
+     * @param index indice dell'oggetto di ricerca
+     * @author Damiano Ficara
+     */
     private ImageIcon toImage(int index) {
         return (ImageIcon) items.get(index).getIcon();
     }
