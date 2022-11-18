5a75a79d8696cd8e2b30d3744f46362379581823
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-11-18 22:02:41 +0100

    doc package

--- clientCV/src/main/java/checkdata/package-info.java
+++ clientCV/src/main/java/checkdata/package-info.java
@@ -1,11 +1,11 @@
 /**
  *
- *  * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
- *  * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
- *  * @author Damiano Ficara - Matricola 744958 - Sede: VA
- *  * @author Luca Perfetti - Matricola 746581 - Sede: VA
+ * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
+ * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
+ * @author Damiano Ficara - Matricola 744958 - Sede: VA
+ * @author Luca Perfetti - Matricola 746581 - Sede: VA
  *
- * il package "CheckData" è una collezione di classi create per controllare determinati dati inseriti dall'utente
+ * il package "checkdata" è una collezione di classi create per controllare determinati dati inseriti dall'utente
  * con determinate regole d'inserimento
  *
  * @see checkdata.CFValidator si occupa di controllare se il codice fiscale inserito rispetti i parametri di un
---
+++ clientCV/src/main/java/graphics/package-info.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
+ * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
+ * @author Damiano Ficara - Matricola 744958 - Sede: VA
+ * @author Luca Perfetti - Matricola 746581 - Sede: VA
+ *
+ * il package "graphics" è una collezione di classi create per la gestione dell'interfaccia grafica
+ *
+ * @see graphics.CurvesPanel si occupa della creazione di un pannello con l'utilizzo di colori gradienti e
+ * animazioni visive della WelcomeScreen
+ *
+ * @see graphics.GradientPanel si occupa della creazione di un pannello con l'utilizzo di colori gradienti
+ *
+ * @see graphics.InfoSearch si occupa della realizzazione di un oggetto avente nome e icona responsabile per il campo SearchField
+ *
+ * @see graphics.InfoSearchEvent si occupa di specificare le informazioni della tipologia di ricerca
+ *
+ * @see graphics.MyPwdField si occupa di definire il campo relativo alla password nel login
+ *
+ * @see graphics.MyTextField si occupa di definire il campo relativo allo username nel login
+ *
+ * @see graphics.RoundButton si occupa di disegnare i bottoni con uno stile circolare
+ *
+ * @see graphics.RoundJTextField si occupa della personalizzazione delle textField presenti nell'interfaccia grafica di tutto il sistema
+ *
+ * @see graphics.SearchField si occupa della gestione grafica del campo di ricerca
+ *
+ */
+
+package graphics;
