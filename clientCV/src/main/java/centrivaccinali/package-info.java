af922172b0b4fe83df604fb845060724b95cfcb0
alessandroCassani <acassani@studenti.uninsubria.it>
2022-11-18 15:30:26 +0100

    documentazione package centrivaccinali

---
+++ clientCV/src/main/java/centrivaccinali/package-info.java
@@ -0,0 +1,27 @@
+/**
+ *  * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
+ *  * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
+ *  * @author Damiano Ficara - Matricola 744958 - Sede: VA
+ *  * @author Luca Perfetti - Matricola 746581 - Sede: VA
+ *
+ * il package "centrivaccinali" e' una collezione di classi che permettono la creazione della GUI
+ * relativa all'operatore vaccinale, inoltre e' presente il punto di avvio (main) dell'applicazione lato client
+ *
+ * @see centrivaccinali.CentriVaccinali in essa e' contenuto il main del programma
+ *
+ * @see centrivaccinali.ServerPointer classe relativa alla memorizzazione dei campi necessari per la creazione della connessione con
+ * il server
+ *
+ * @see centrivaccinali.UIChoosingRooles UI dove l'utente si identifica, ovvero se e' un operatore vaccinale
+ * o un cittadino
+ *
+ * @see centrivaccinali.UIRegisterVaccinated UI dove e' possibile inserire le informazioni di un vaccinato da registrare
+ *
+ * @see centrivaccinali.UIRegisterVaxCenter UI dove e' possibile inserire le informazioni di un centro vaccinale da registrare
+ *
+ * @see centrivaccinali.UIVaccineOperator UI dove sono fruibili le feature di un operatore vaccinale
+ *
+ * @see centrivaccinali.WelcomeScreen UI di avvio software
+ */
+
+package centrivaccinali;
