1794cc8b566430eefe6e413226a02a0325df60a1
alessandroCassani <acassani@studenti.uninsubria.it>
2022-11-21 16:31:46 +0100

    documentazione package cittadini

---
+++ clientCV/src/main/java/cittadini/package-info.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
+ * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
+ * @author Damiano Ficara - Matricola 744958 - Sede: VA
+ * @author Luca Perfetti - Matricola 746581 - Sede: VA
+ *
+ * il package cittadini contiene le classi relative alle  UI relative alle funzionalita' disponibili allo user cittadino
+ *
+ * @see cittadini.UIAdverseEvent UI per inserimentro evento avverso se non precedentemente registrato, altrimenti visualizzazione
+ * evento avverso
+ *
+ * @see cittadini.UICitizen UI relativa a dashboard cittadino
+ *
+ * @see cittadini.UILogin UI relativa ad operazione login per accedere e registrare/visualizzare evento avverso
+ *
+ * @see cittadini.UIRegisterCitizen UI relativa a registrazione del cittadino a sistema
+ *
+ * @see cittadini.UISearchVaxCenter Ui relativa alla ricerca di un centro vaccinale e visualizzazione prospetto riassuntivo
+ *
+ * @see cittadini.UISearchVaxCenterDialog UI relativa al dialog per la visualizzazione del prospetto riassuntivo del cengtro vaccinal
+ * selezionato
+ *
+ */
+
+package cittadini;
