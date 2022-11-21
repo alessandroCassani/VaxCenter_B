026aa6d3a61b552501e5f86504f4e87c8e40858e
alessandroCassani <acassani@studenti.uninsubria.it>
2022-11-21 16:37:27 +0100

    documentazione package database

---
+++ serverCV/src/main/java/database/package-info.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author Alessandro Cassani - Matricola: 744512 - Sede: VA
+ * @author Paolo Bruscagin - Matricola: 744703 - Sede: VA
+ * @author Damiano Ficara - Matricola 744958 - Sede: VA
+ * @author Luca Perfetti - Matricola 746581 - Sede: VA
+ *
+ * Il package database consiste nell'insieme di classi relative alla creazione e gestione del DB, e l'implementazione dei metodi
+ * del server che offrono le funzionalita' richieste accedendo al relativo DB
+ *
+ * @see database.CapProvincia classe di appoggio utile nella creazione di un oggetto contenete il cap e la provincia di un comune
+ *
+ * @see database.DBManagement classe relativa alla gestione e creazione del DB.
+ *
+ * @see database.Server_CentriVaccinali classe dove è contenuto il main del programma lato server
+ *
+ * @see database.ServerImpl classe in cui sono contenute le implementazioni dei metodi che rappresentano le funzionalita' richieste dagli
+ * utenti
+ *
+ * @see database.ServerInterface interfaccia remota dove sono contenute le segnature dei metodi del server
+ *
+ */
+
+package database;
